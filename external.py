8c241e15989c8eba2f65714a38fee859406a7226
GrigorievDmitry <36789339+GrigorievDmitry@users.noreply.github.com>
2020-03-07 19:03:59 +0300

    Update external.py

--- external.py
+++ external.py
@@ -5,5 +5,8 @@ Created on Sat Mar  7 17:11:21 2020
 @author: HYPERPC
 """
 
-def responder(inp):
-    return "Responding"
+def grader(week, answer):
+    import requests
+    resp = requests.get("https://colabconnectionhost.herokuapp.com/" + str(week),
+                       params={"answer": answer})
+    return resp.text
